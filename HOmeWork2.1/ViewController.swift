b6a5a082f7f13c0f6c3b26da78c3c600bd575c6c
ShevtsovVA <v.a.shevtsov@yandex.ru>
2021-05-18 22:12:00 +0300

    Initial Commit

---
+++ HOmeWork2.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HOmeWork2.1
+//
+//  Created by Евгения Шевцова on 18.05.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
